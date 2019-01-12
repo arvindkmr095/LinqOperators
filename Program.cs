25eb752033cbf492b0f023de85836e745c961f7a
Arvind Kumar <arvindkmr095@gmail.com>
2019-01-12 11:05:56 +0530

    Create Empty Console App

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
